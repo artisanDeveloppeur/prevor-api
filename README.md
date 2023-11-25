9619b95fc3d70c25df9769a42b3ccdd9f6e00c82
Denis <DenisArtisanDeveloppeur@gmail.com>
2023-11-25 09:40:08 +0100

    test api

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# prevor-api
+# Prevor Test développement
+
+    II. TEST DE DEVELOPPEMENT.
+
+    4) Pour un projet interne, nous avons besoin d’intégrer sur un de nos sites l’emplacement des défibrillateurs de la ville de Liège. Les données sont disponibles sur la page «  Les défibrillateurs — Open Data Liège (liege.be) » et ne doivent pas être présentées sur une carte :
+
+        a. Trouvez le moyen le plus efficient de récupérer ces données afin de les utiliser.
+        b. Expliquez vos choix techniques et comment les implémenter.
+
+    5) Réalisez une page web simple mettant en œuvre les points de la question 4.
+
+----------------------------------------------------------------------------------
+
+Documentation code file (scripts/api.js)
+
+(data = https://opendata.liege.be/api/explore/v2.1/catalog/datasets/defibrillateurs/records?limit=40)
+
+1. GET request using fetch(data)  
+2. converting received data to JSON
+3. Create a variable to store HTML table headers
+4. Loop through each data and add a table row
+5. DOM Display result
+
+
---
+++ scripts/api.js
@@ -0,0 +1,140 @@
+
+
+// 1. GET request using fetch()
+fetch("https://opendata.liege.be/api/explore/v2.1/catalog/datasets/defibrillateurs/records?limit=40")
+  // Converting received data to JSON
+  .then((response) => response.json())
+  .then((json) => {
+    console.log(json.results)
+
+    // 2. Create a variable to store HTML table headers
+    let li = `<tr>
+    <th>Identifiant</th>
+    <th>Street name</th>
+    <th>House number</th>
+    <th>Postal code</th>
+    <th>Floor</th>
+    <th>Created</th>
+    <th>Last modified</th>
+    <th>Street code</th>
+    <th>Icar address (ID) </th>
+    <th>Icar street (ID)</th>
+    <th class="hidden">City</th>
+    <th class="hidden">Provider</th>
+    </tr>`;
+
+    // 3. Loop through each data and add a table row
+    json.results.forEach((defibrillateurs) => {
+      console.log(defibrillateurs)
+      if (defibrillateurs.floor == null) { return " " }
+      li += `<tr>
+      <td>${defibrillateurs.gid}</td>
+      <td>${defibrillateurs.street_name}</td>
+      <td>${defibrillateurs.house_number}</td>
+      <td>${defibrillateurs.postal_code}</td>
+      <td>${defibrillateurs.floor}</td>
+      <td>${defibrillateurs.created}</td>
+      <td>${defibrillateurs.last_modified}</td>
+      <td>${defibrillateurs.street_code}</td>
+      <td>${defibrillateurs.icar_address_id}</td>
+      <td>${defibrillateurs.icar_street_id}</td>
+      <td class="hidden">${defibrillateurs.city}</td>
+      <td class="hidden">${defibrillateurs.provider}</td>
+    </tr>`;
+    });
+
+    // 4. DOM Display result
+    document.getElementById("defibrillateurs").innerHTML = li;
+
+  });
+
+/*
+{
+"gid":"4286",
+"provider":"SIPPT",
+"city":"Liège",
+"street_name":"boulevard de la Constitution",
+"house_number":"82",
+"additional_details":"Salle des sports - - Salle des sports",
+"geo_point_2d":{
+"lon":5.5918270747,
+"lat":50.644958397
+},
+"geo_shape":{
+"type":"Feature",
+"geometry":{
+"coordinates":[
+5.5918270747,
+50.644958397
+],
+"type":"Point"
+},
+"properties":{
+}
+},
+"street_code":6016,
+"icar_address_id":1137235,
+"created":"2021-10-20",
+"last_modified":"2021-10-20",
+"floor":"Salle des sports",
+"postal_code":4020,
+"icar_street_id":7729676
+},
+*/
+/*
+  Identifiant
+  2706
+  Gestionnaire
+  SIPPT
+  Ville
+  Liège
+  Rue
+  rue Burenville
+  Numéro
+  46
+  Complément d'information
+  Local des professeurs - - Local des professeurs
+  Code rue (lisrue)
+  4 024
+  Identifiant d'adresse (ICAR / BeStaddress)
+  1 109 362
+  Date de création
+  12 juillet 2023
+  Date de dernière modification
+  12 juillet 2023
+  Étage
+  Local des professeurs
+  Code postal
+  4 000
+  Identifiant de la rue (ICAR / BeStaddress)
+  7 730 235
+*/
+
+
+// main.js
+
+// 5. POST request using fetch()
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  // 6. Adding method type
+  method: "POST",
+
+  // 7. Adding body or contents to send JSON Stringify
+  body: JSON.stringify({
+    title: "foo",
+    body: "bar",
+    userId: 1
+  }),
+
+  // 8. Adding headers to the request
+  headers: {
+    "Content-type": "application/json; charset=UTF-8"
+  }
+})
+  // 9. Converting to JSON
+  .then((response) => response.json())
+
+  // 10. Displaying results to console
+  .then((json) => console.log(json));
+
+
+
