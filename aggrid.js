aab9e0d900664865e4f6d63e4f6fe5b35faafd19
Denis <DenisArtisanDeveloppeur@gmail.com>
2023-12-12 21:08:13 +0100

    default settings

--- aggrid.js
+++ aggrid.js
@@ -4,27 +4,52 @@ let gridApi;
 const gridOptions = {
   columnDefs: [
     { field: 'gid' },
-    { field: 'street_name', minWidth: 100 },
-    { field: 'house_number', minWidth: 100 },
-
-    { field: 'street_code' },
-    { field: 'icar_address_id' },
-    { field: 'created' },
-    { field: 'last_modified' },
+    { headerName: 'Street name', field: 'street_name', minWidth: 250 },
+    { headerName: 'House number', field: 'house_number' },
+    { headerName: 'Postal code', field: 'postal_code' },
     { field: 'floor' },
-    { field: 'postal_code' },
-    { field: 'icar_street_id' },
+    { headerName: 'Street code', field: 'street_code' },
+    { headerName: 'ID icar address', field: 'icar_address_id' },
+    { headerName: 'ID icar street', field: 'icar_street_id' },
+
+
+
+
 
     /*
     { field: 'provider' },
     { field: 'city' },
     { field: 'additional_details' },
     { field: 'geo_point_2d.lat' },
+    { field: 'geo_shape.geometry.coordinates' },
+    { field: 'created' },
+    { field: 'last_modified' },
+    
     */
   ],
+  /*
   defaultColDef: {
     flex: 1,
   },
+  */
+  defaultColDef: {
+    // set the default column width
+    width: 150,
+    // make every column editable
+    editable: false,
+    // make every column use 'text' filter by default
+    filter: 'agTextColumnFilter',
+    // enable floating filters by default
+    floatingFilter: true,
+    // disable cell data types
+    cellDataType: false,
+  },
+
+  // default ColGroupDef, get applied to every column group
+  defaultColGroupDef: {
+    marryChildren: true,
+  },
+
 };
 
 function onFilterTextBoxChanged() {
@@ -40,7 +65,7 @@ document.addEventListener('DOMContentLoaded', function () {
   gridApi = agGrid.createGrid(gridDiv, gridOptions);
   //console.log(gridApi)
   //fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
-  fetch('https://opendata.liege.be/api/explore/v2.1/catalog/datasets/defibrillateurs/records?limit=40')
+  fetch('https://opendata.liege.be/api/explore/v2.1/catalog/datasets/defibrillateurs/records?limit=100')
     .then((response) => response.json())
     .then((data) => gridApi.setGridOption('rowData', data.results));
 });
